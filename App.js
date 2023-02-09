8e276b041d5fe2fe4333099aae92e7baf54d27ff
FPacherres <fpacherres.tech@gmail.com>
2023-02-09 01:55:14 -0500

    firebase

--- App.js
+++ App.js
@@ -3,11 +3,70 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import MyDrawer from './src/routes';
 
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { initializeApp } from 'firebase/app'
+import { firebaseConfig } from './firebase-config';
+import { View, Text, TouchableOpacity, TextInput, Alert } from 'react-native';
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <MyDrawer />
-    </NavigationContainer>
+    <LoginScreen />
+    // <NavigationContainer>
+    //   <MyDrawer />
+    // </NavigationContainer>
   );
 }
 
+function LoginScreen() {
+
+  const [email, SetEmail] = React.useState('')
+  const [password, SetPassword] = React.useState('')
+
+  const app = initializeApp(firebaseConfig)
+  const auth = getAuth(app)
+
+  const handleCreateAccount = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential)=> {
+      console.log('Account created')
+      const user = userCredential.user
+      console.log(user)
+    })
+    .catch(error => {
+      console.log(error)
+      Alert.alert(error.message)
+    })
+  }
+
+  const handleSignIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      console.log('Signed in!')
+      const user = userCredential.user
+      console.log(user)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  return( 
+    <View style={{paddingTop: 200, paddingLeft: 60}}>
+      <Text>Login</Text>
+      <View>
+        <Text>Email</Text>
+        <TextInput style={{backgroundColor:"#990000", width: 200, marginTop: 50, height: 50}}  onChangeText={ (text) => SetEmail(text) } />
+      </View>
+      <View>
+        <Text style={{marginTop: 50}} >Password</Text>
+        <TextInput style={{backgroundColor:"#330000", width: 200, height: 50}}  onChangeText={ (text) => SetPassword(text) } />
+      </View>
+      <TouchableOpacity style={{backgroundColor:"#FF0000", width: 100, marginTop: 50, height: 30}} onPress={handleSignIn}>
+        <Text>Ingresar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{backgroundColor:"#00FF00", width: 100, marginTop: 50, height: 30}} onPress={handleCreateAccount}>
+        <Text>Crear cuenta</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBE8ZxTPJd2PMiGdeLk7gIPthc-X7D4IGg",
+  authDomain: "rent-control-901e9.firebaseapp.com",
+  projectId: "rent-control-901e9",
+  storageBucket: "rent-control-901e9.appspot.com",
+  messagingSenderId: "191923859489",
+  appId: "1:191923859489:web:92320150a8c46abed67360"
+};
